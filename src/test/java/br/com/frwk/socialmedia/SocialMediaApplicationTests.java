4779980a7dde64e20c15e01f1b498ee08d06e0e9
Victor Pietro <victor_pietro@hotmail.com>
2021-05-02 08:39:14 -0300

    first commit

---
+++ src/test/java/br/com/frwk/socialmedia/SocialMediaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.frwk.socialmedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
